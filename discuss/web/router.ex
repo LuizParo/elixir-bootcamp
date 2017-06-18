537d3d6019729d27b14122e5abba17c393327430
Luiz Guilherme Paro <lg_paro@hotmail.com>
2017-06-18 11:48:12 -0300

    Creating the comments table and making the relatioship with topic and user

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -19,6 +19,7 @@ defmodule Discuss.Router do
 
         get "/", TopicController, :index
         get "/topics/new", TopicController, :new
+        get "/topics/:id", TopicController, :show
         get "/topics/:id/edit", TopicController, :edit
         post "/topics", TopicController, :create
         put "/topics/:id", TopicController, :update
